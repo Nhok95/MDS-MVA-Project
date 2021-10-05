cbf9da108d0df4eaa258de47f0fe4d21d2ae7432
Nhok95 <enrique_1095@hotmail.es>
2021-10-05 16:10:16 +0200

    Little changes in Imputation script

--- Imputation.R
+++ Imputation.R
@@ -81,6 +81,57 @@ str(bikeDataSet)
 
 
 ### First data imputation with MICE ###
+checkMeanDiff = function(original, bikeDS_imp, bikeDataSet) {
+  diff = c()
+  cnames = colnames(original[c(6,8,9,10,11,12,13,14)])
+  
+  for (i in c(1:length(cnames))) {
+    actual = original[,cnames[i]][is.na(bikeDataSet[,cnames[i]])]
+    predicted = bikeDS_imp[,cnames[i]][is.na(bikeDataSet[,cnames[i]])]
+    print("###")
+    print(sprintf("--column: %s", cnames[i]))
+    print(mean(actual))
+    print(mean(predicted))
+    
+    diff = append(diff,mean(actual) - mean(predicted))
+    
+    print(sprintf("----diff: %s ", diff[i]))
+  }
+  
+  return(diff)
+}
+
+checkMeanDiff2 = function(original, bikeDS_imp) {
+  diff = c()
+  cnames = colnames(original[c(6,8,9,10,11,12,13,14)])
+  
+  for (i in c(1:length(cnames))) {
+    actual = original[,cnames[i]]
+    predicted = bikeDS_imp[,cnames[i]]
+    print("###")
+    print(sprintf("--column: %s", cnames[i]))
+    print(mean(actual))
+    print(mean(predicted))
+    
+    diff = append(diff,mean(actual, na.rm=T) - mean(predicted, na.rm=T))
+    
+    print(sprintf("----diff: %s ", diff[i]))
+  }
+  
+  return(diff)
+}
+
+imp=mice(bikeDataSet,m=5)
+bikeDS_imp=complete(imp)
+length(which(is.na(bikeDS_imp)))
+
+
+diff1 = checkMeanDiff(original, bikeDS_imp, bikeDataSet)
+diff1.2 = checkMeanDiff2(original, bikeDS_imp)
+
+
+
+
 init = mice(bikeDataSet, maxit=0)
 meth = init$method
 predM = init$predictorMatrix
@@ -88,34 +139,49 @@ predM = init$predictorMatrix
 predM[c("Id"),]=0
 predM[,c("Id")]=0
 
-predM[c("Day"),]=0
-predM[,c("Day")]=0
+#predM[c("Day"),]=0
+#predM[,c("Day")]=0
 
-predM[c("Month"),]=0
-predM[,c("Month")]=0
+#predM[c("Month"),]=0
+#predM[,c("Month")]=0
 
-predM[c("Year"),]=0
-predM[,c("Year")]=0
+#predM[c("Year"),]=0
+#predM[,c("Year")]=0
 
-predM[c("Date"),]=0
-predM[,c("Date")]=0
+#predM[c("Date"),]=0
+#predM[,c("Date")]=0
 
-meth[c("Temperature")]="norm"
-meth[c("Humidity")]="norm"
+#meth[c("Rented.Bike.Count")]="norm"
+#meth[c("Temperature")]="norm"
+#meth[c("Humidity")]="norm"
+#meth[c("Wind.Speed")]="norm"
+#meth[c("Visibility")]="norm"
+#meth[c("Dew.Point.Temperature")]="norm"
+#meth[c("Solar.Radiation")]="norm"
+#meth[c("Rainfall")]="norm"
+
 
 predM; meth
 
 k = 5
 imp=mice(bikeDataSet,m=k, method=meth, predictorMatrix = predM)
 
+bikeDS_imp2=complete(imp)
 
+diff2 = checkMeanDiff(original, bikeDS_imp2, bikeDataSet)
+diff2.2 = checkMeanDiff2(original, bikeDS_imp2)
 
-bikeDS_imp=complete(imp)
-original[c(1,2),]
-bikeDataSet[c(1,2),]
-bikeDS_imp[c(1,2),]
+diff1
+diff2
 
+diff1.2
+diff2.2
 
+diff.df = data.frame(diff1,diff2, row.names=cnames)
+diff.df = as.data.frame(t(diff.df))
+
+diff.df.2 = data.frame(diff1.2,diff2.2, row.names=cnames)
+diff.df.2 = as.data.frame(t(diff.df.2))
 
 #########################################################
 for (j in c(1:ncol(winter_outliers))) {
