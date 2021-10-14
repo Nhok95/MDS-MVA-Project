e9a86743f9671ad00a14f250adc41761aa5f29cb
Carlos Moyano <carlosmoygra@hotmail.com>
2021-10-14 22:29:40 +0200

    Now robust mahalanobis is used for multivariate outliers detection.
    Code inside Multivariate...._Malahanobis.R has changed, also a txt file with the ID of the multivariate outliers detected has been created.

--- MultivariateOutlierDetection_Mahalanobis.R
+++ MultivariateOutlierDetection_Mahalanobis.R
@@ -47,16 +47,16 @@ print(lapply(bikeDataSet_no_factors_no_id, mad))
 bikeDataSet_no_factors_no_id <- bikeDataSet_no_factors_no_id[, !names(bikeDataSet_no_factors_no_id) %in% c("Id", "Rainfall", "Snowfall", "Solar.Radiation")]
 describe(bikeDataSet_no_factors_no_id)
 
-# Calculation of the Classical Mahalanobis Distance
+# Calculation of the Classical Mahalanobis Distance & Robust Mahalanobis Distance
 mout_res <- Moutlier(bikeDataSet_no_factors_no_id, quantile = 0.975, plot = FALSE)
 
+## Outliers using Classical Mahalanobis Distance
 plot(mout_res$md,
      main = "Classical Mahalanobis distance",
      ylab = "Classical mahalanobis distance value")
 cutoff <- rep(mout_res$cutoff, nrow(bikeDataSet_no_factors_no_id))
-cutoff_extreme <- rep(mout_res$cutoff, nrow(bikeDataSet_no_factors_no_id))
 lines(cutoff, col = "red")
-cutoff_extreme_value <- 10
+cutoff_extreme_value <- 5
 cutoff_extreme <- rep(cutoff_extreme_value, nrow(bikeDataSet_no_factors_no_id))
 lines(cutoff_extreme, col = "blue")
 
@@ -64,4 +64,23 @@ lines(cutoff_extreme, col = "blue")
 # since they're multidimensional outliers
 extreme_outliers <- which(mout_res$md > cutoff_extreme)
 extreme_outliers
-# 4064, 4132, 4133, 4134, 4135, 4254, 4256, 4277, 4278, 4279, 4280, 4364
+length(extreme_outliers)
+# Number of outliers using Classical Mahalanobis Distance: 73
+
+## Outliers using Robust Mahalanobis Distance
+plot(mout_res$rd,
+     main = "Robust Mahalanobis distance",
+     ylab = "Robust mahalanobis distance value")
+lines(cutoff, col = "red")
+cutoff_extreme_value <- 20
+cutoff_extreme <- rep(cutoff_extreme_value, nrow(bikeDataSet_no_factors_no_id))
+lines(cutoff_extreme, col = "blue")
+
+# These indices of the dataframe need an imputation of values for all their variables
+# since they're multidimensional outliers
+extreme_outliers <- which(mout_res$rd > cutoff_extreme)
+extreme_outliers
+length(extreme_outliers)
+# Number of outliers using Robust Mahalanobis Distance: 87
+# We will only consider multivariate outliers based on Robust Mahalanobis Distance
+fwrite(list(extreme_outliers), file = paste("multivariate_outliers_rmd", ".txt", sep=""))
