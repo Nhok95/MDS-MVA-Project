c02b0c5556e951761c2d044753a78bbab21fbcc1
Nhok95 <enrique_1095@hotmail.es>
2022-01-15 10:05:48 +0100

    AssociationRules

--- AssociationRules.R
+++ AssociationRules.R
@@ -1,7 +1,6 @@
 #####################################
 ##            Ass. Rules           ##
 #####################################
-install.packages("arules")
 
 library(rstudioapi)
 library(stringr)
@@ -10,11 +9,21 @@ library(arulesViz)
 library(dplyr)
 library(datasets)
 
+library(corrplot)
+
 library(FactoMineR)
 library(factoextra)
 
 rm(list=ls(all=TRUE))
 set.seed(123)
+
+## Function
+calcQ <- function(x) {
+  s.x <- summary(x)
+  iqr<-s.x[5]-s.x[2]
+  list(souti=s.x[2]-3*iqr, mouti=s.x[2]-1.5*iqr, min=s.x[1], q1=s.x[2], q2=s.x[3], 
+       q3=s.x[5], max=s.x[6], mouts=s.x[5]+1.5*iqr, souts=s.x[5]+3*iqr ) 
+}
 
 current_path <- getActiveDocumentContext()$path
 setwd(dirname(current_path))
@@ -22,20 +31,90 @@ setwd(dirname(current_path))
 # Read the dataset which has the outliers indicated
 load("bikeDataSet.RData")
 
-bikeDataSetClean = bikeDataSet[which(bikeDataSet$mout == "NoMOut"),]
-bikeDataSetClean = select(bikeDataSetClean, -c("Id", "Day", "Month", "Year", "mout"))
+bikeDataSetNoMout = bikeDataSet[which(bikeDataSet$mout == "NoMOut"),]
+
+bikeDataSetClean.num = select(bikeDataSetNoMout, c("Temperature",
+                                               "Humidity",
+                                               #"Wind.Speed",
+                                               #"Visibility",
+                                               #"Dew.Point.Temperature",
+                                               "Solar.Radiation",
+                                               "Rainfall",
+                                               "Snowfall",
+                                               "Rented.Bike.Count"))
+
+# More or less independent variables
+corrplot(cor(bikeDataSetClean.num), method="number", type="lower", bg='lightslategray') 
+
+bikeDataSetClean = select(bikeDataSetNoMout, c("Temperature",
+                                               "Humidity",
+                                               #"Rainfall",
+                                               #"Snowfall",
+                                               "Month",
+                                               "Hour",
+                                               #"Season",
+                                               "Rented.Bike.Count"))
+
+# Convert first 5 numeric vars to Factor
+
+str(bikeDataSetClean)
+
+for (i in 1:3) {
+  if (i < 3) { #Temperature, Humidity
+    quartiles = calcQ(bikeDataSetClean[,i])
+    bikeDataSetClean[,i] <- cut(bikeDataSetClean[,i],
+                                breaks = c(quartiles$min, quartiles$q1, quartiles$q2, quartiles$q3, quartiles$max),
+                                include.lowest = TRUE)
+    #levels(bikeDataSetClean[,i]) = c("low", "mid", "high", "v-high")
+    # table(bikeDataSetClean[,i])
+  }
+  # } else { # Rainfall, Snowfall
+  #   quartiles = calcQ(bikeDataSetClean[,i]);
+  #   bikeDataSetClean[,i] <- factor(as.logical(bikeDataSetClean[,i]))
+  #   levels(bikeDataSetClean[,i]) = c("No", "Yes")
+  # }
+  
+}
+
+# Factor target
+RBC.q = calcQ(bikeDataSetClean$Rented.Bike.Count)
+bikeDataSetClean$Rented.Bike.Count <- cut(bikeDataSetClean$Rented.Bike.Count,
+                                      breaks = c(RBC.q$min, RBC.q$q1, RBC.q$q2, RBC.q$q3, RBC.q$max),
+                                      include.lowest = TRUE)
+levels(bikeDataSetClean$Rented.Bike.Count) = c("low", "mid", "high", "v-high")
+table(bikeDataSetClean$Rented.Bike.Count)
+
+names(bikeDataSetClean)[names(bikeDataSetClean) == "Rented.Bike.Count"] <- "RB"
+
+str(bikeDataSetClean)
+
+
+############## Association Rules Analysis ##############
+
 
 rdata = transactions(bikeDataSetClean)
-itemFrequencyPlot(rdata, topN = 10)
+inspect(head(rdata))
 
-rules = apriori(data = rdata, parameter = list(support = 0.05, confidence = 0.9))
+itemFrequencyPlot(rdata, support = 0.01, cex.names=0.8, topN = 5)
 
+rules = apriori(data = rdata, parameter = list(support = 0.005, confidence = 0.85))
 summary(rules)
-inspect(head(rules))
+inspect(head(rules,by = "confidence"))
 
-strongrules = subset(rules, subset = lift > 1.2)
+rules.target.low <- subset(rules, subset = rhs %in% "RB=low" & lift > 1.5)
+rules.target.mid <- subset(rules, subset = rhs %in% "RB=mid" & lift > 1.5)
+rules.target.high <- subset(rules, subset = rhs %in% "RB=high" & lift > 1.5)
+rules.target.vhigh <- subset(rules, subset = rhs %in% "RB=v-high" & lift > 1.5)
 
-inspect(head(strongrules,15))
+inspect(rules.target.low,by = "lift")
+inspect(rules.target.mid,by = "lift")
+inspect(rules.target.high,by = "lift")
+inspect(rules.target.vhigh,by = "lift")
+
+# inspect(head(rules.target.low,by = "lift",10))
+# inspect(head(rules.target.mid,by = "lift",10))
+# inspect(head(rules.target.high,by = "lift",10))
+# inspect(head(rules.target.vhigh,by = "lift",10))
 
 ## Convert features into items using R
 
